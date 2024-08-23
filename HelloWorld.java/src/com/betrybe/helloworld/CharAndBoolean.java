0e2a210466307e088a273aff122f22b840d0782b
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-23 18:31:16 -0300

    mais primitivos

--- HelloWorld.java/src/com/betrybe/helloworld/CharAndBoolean.java
+++ HelloWorld.java/src/com/betrybe/helloworld/CharAndBoolean.java
@@ -2,8 +2,8 @@ package com.betrybe.helloworld;
 
 public class CharAndBoolean {
     public static void main(String[] args) {
-    char letraD = 'd'; //metodo char é sempre case sensitive
-    char outraLetraD = 100;
+    char letraD = 'c'; //metodo char é sempre case sensitive
+    char outraLetraD = 99;
 
         System.out.println(letraD == outraLetraD);
     }
