2f643ecd26f9c45268933942df97e2af5de0960e
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-23 18:29:56 -0300

    mais tipos primitivos

---
+++ HelloWorld.java/src/com/betrybe/helloworld/CharAndBoolean.java
@@ -0,0 +1,10 @@
+package com.betrybe.helloworld;
+
+public class CharAndBoolean {
+    public static void main(String[] args) {
+    char letraD = 'd'; //metodo char é sempre case sensitive
+    char outraLetraD = 100;
+
+        System.out.println(letraD == outraLetraD);
+    }
+}
