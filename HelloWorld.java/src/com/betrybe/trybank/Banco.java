a879ebfc24df9f3c9c4291a463622f13225fa0e9
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-25 09:16:43 -0300

    criando uma "conta bancaria"

--- HelloWorld.java/src/com/betrybe/trybank/Banco.java
+++ HelloWorld.java/src/com/betrybe/trybank/Banco.java
@@ -2,6 +2,13 @@ package com.betrybe.trybank;
 
 public class Banco {
     public static void main(String[] args) {
-        Banco conta = new Banco();
+    ContaBancaria conta = new ContaBancaria();
+
+    conta.mostraMensagem();
+    conta.depositar(500);
+
+    double valorConvertido = conta.converteMoeda(30, 5);
+        System.out.println(valorConvertido);
     }
+
 }
